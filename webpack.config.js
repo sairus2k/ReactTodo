2d0777693088f75c6b4870a51e828b361fb9a5de
Anton Savoskin <anton@sliday.com>
2016-11-29 21:26:30 +0700

    Add showCompleted and searchText reducers

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = {
       'node_modules',
       './app/components',
       './app/api',
-      './app/actions'
+      './app/actions',
+      './app/reducers'
     ],
     alias: {
       Main: 'app/components/Main.jsx'
